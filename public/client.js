7eb705e72ad822d7e212bb1aefe1b55f9a6eedb1
Antoineb3 <antoineb374@gmail.com>
2020-11-13 14:54:57 -0700

    almost done

--- public/client.js
+++ public/client.js
@@ -1,12 +1,88 @@
 $(function () {
     let socket = io();
+    let user = '';
+    let chat_log = [];
+    let user_list = [];
+
+    // Set the name of this user
+    socket.on('set user', function (msg) {
+        console.log('setting user name: ');
+        console.log(msg);
+        user = msg;
+    });
+
+    // update the user list
+    socket.on('set user list', function (msg) {
+        console.log('setting user list: ');
+        console.log(msg);
+        user_list = msg;
+
+        // Render the new user list
+        $('#users').empty();
+        user_list.forEach(element => {
+            let li = $('<li>').text(element.name).css("color", '#' + element.color);
+            if (element.name === user) {
+                li.append('(you)');
+                li.css('font-weight', 'bold');
+            }
+            $('#users').append(li);
+        });
+    });
+
+    // update the chat log
+    socket.on('set chat log', function (msg) {
+        // window.scrollTo(0, document.body.scrollHeight);
+        console.log('setting chat log list: ');
+        console.log(msg);
+        chat_log = msg;
+
+        // Render the new chat log 
+        $('#messages').empty();
+        chat_log.forEach(element => {
+            let li = $('<li>').addClass('message');
+            let date = new Date(element.date);
+            if (element.user.name !== 'SERVER') {
+                li.append($('<span>').text(formatAMPM(date) + ' ').addClass('time'));
+                li.append($('<span>').text(element.user.name + ': ').css("color", '#' + element.user.color).addClass('user'));
+            }
+            li.append($('<span>').text(element.msg).addClass('msg'));
+            if (element.user.name === user) {
+                li.css('font-weight', 'bold');
+            }
+            $('#messages').append(li);
+        });
+    });
+
+
+    // message entered
     $('form').submit(function () {
-        socket.emit('chat message', $('#m').val());
+        // Remove leading and trailing white spaces
+        let msg = $('#m').val().trim();
+
+        if (msg.startsWith('/name ')) { // name command
+            socket.emit('user command', msg.replace('/name ', ''));
+        }
+        else if (msg.startsWith('/color ')) { // color command 
+            socket.emit('color command', msg.replace('/color ', ''));
+        }
+        else if (msg) { // Normal, not empty message
+            socket.emit('chat message', msg);
+        }
         $('#m').val('');
         return false;
     });
-    socket.on('chat message', function (msg) {
-        $('#messages').append($('<li>').text(msg));
-        window.scrollTo(0, document.body.scrollHeight);
-    });
+
+    // convert am/pm format: https://www.codegrepper.com/code-examples/delphi/time+display+24+hour+clock+format+js
+    function formatAMPM(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        let strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+
+
 });
--- server.js
+++ server.js
@@ -3,22 +3,100 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 let port = process.env.PORT || 3000;
+let emojiMap = require('smile2emoji').emojiMap; // https://www.npmjs.com/package/smile2emoji
+
+//Order emoji map by key lengh to match longer emojies before shorter ones (e.g. >:( before :( )
+const orderedEmojiMap = {};
+Object.keys(emojiMap).sort((a, b) => b.length - a.length).forEach(key => orderedEmojiMap[key] = emojiMap[key]);
+
+let chat_log = [];
+let user_list = [];
+let usercount = 1;
+
+// Rplce all for strings, needed because replace only replaces first occurance
+// https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string
+String.prototype.replaceAll = function (find, replace) {
+  var str = this;
+  return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
+};
 
 // Serve static resources
 app.use(express.static('public'))
 
-// Serve client side socket.io
-app.get('/socket.io.min.js', function (req, res) {
-  res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.min.js');
-});
-app.get('/socket.io.min.js.map', function (req, res) {
-  res.sendFile(__dirname + '/node_modules/socket.io/client-dist/socket.io.min.js.map');
-});
-
 io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg);
+  // add user on connect
+  // Random color https://css-tricks.com/snippets/javascript/random-hex-color/
+  let user = { name: ('User' + (usercount++)), color: (Math.floor(Math.random() * 16777215).toString(16)) };
+  socket.emit('set user', user.name);
+  // update user list for everyone on connect
+  user_list.push(user);
+  io.emit('set user list', user_list);
+  // add connection message to chat log
+  sendMessage(user.name + ' connected');
+
+  // remove user on disconnect
+  socket.on('disconnect', function () {
+    user_list = user_list.filter(item => item.name != user.name);
+    io.emit('set user list', user_list);
+    sendMessage(user.name + ' disconnected');
   });
+
+  // when new chat message is sent
+  socket.on('chat message', function (msg) {
+    // Replace emojis found in emoji map
+    Object.keys(orderedEmojiMap).forEach(key => msg = msg.replaceAll(key, emojiMap[key]));
+    sendMessage(msg, user);
+  });
+
+  // update user name
+  socket.on('user command', function (msg) {
+    msg = msg.trim();
+    if (nameTaken(msg)) { // name taken
+      return; //TODO: send error
+    }
+    else if (msg == 'SERVER') { // reserved name
+      return; //TODO: send error
+    }
+    // upadate state
+    user.name = msg;
+    socket.emit('set user', user.name);
+    io.emit('set user list', user_list);
+    io.emit('set chat log', chat_log);
+  });
+
+  // update user color
+  socket.on('color command', function (msg) {
+    msg = msg.trim();
+    // https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation/8027444
+    if (typeof msg === 'string' && msg.length === 6 && !isNaN(Number('0x' + msg))) {
+      user.color = msg;
+      io.emit('set user list', user_list);
+      io.emit('set chat log', chat_log);
+    }
+    else {
+      // TODO: error message
+    }
+  });
+
+
+  // Helper functions
+
+  // Adds a message to the log and sends it to all users, sender is server when not specified
+  function sendMessage(msg, user = { name: 'SERVER', color: '000000' }) {
+    // only keep 200 most recent
+    if (chat_log.length >= 200) {
+      chat_log.shift();
+    }
+    let date = new Date();
+    chat_log.push({ date, user, msg });
+    io.emit('set chat log', chat_log);
+  }
+
+  // Checks if a name has already been taken
+  function nameTaken(name) {
+    return user_list.filter(u => u.name == name).length > 0;
+  }
+
 });
 
 http.listen(port, function () {
